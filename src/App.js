89598e15b89ca495292076fabd512455e778d30a
yuvalta <tamir.yuval1@gmail.com>
2022-03-19 19:55:37 +0200

    Added start, stop, upsert buttons

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import React, {useEffect, useState} from "react";
 
 function App() {
   const [userEmail, setUserEmail] = useState("");
+  const [assetUrl, setAssetUrl] = useState("");
   const [isLoggedIn, setLoggedIn] = useState(false);
   const [assetsList, setAssetsList] = useState("");
 
   useEffect(() => {
-    fetch('https://alert-scraper.herokuapp.com/get_assets_for_user?user_email=tamir.yuval1@gmail.com')
+    fetch('https://alert-scraper.herokuapp.com/get_assets_for_user?user_email=' + userEmail)
       .then(response => response.json())
       .then(data => setAssetsList(data));
-  }, []);
+  }, [userEmail, assetUrl]);
 
   return (
     <div className="App">
@@ -21,7 +22,7 @@ function App() {
           clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
           buttonText="Login"
           onSuccess={(response) => {
-            console.log(response);
+            console.log(response.Du.tv);
             setLoggedIn(true)
             setUserEmail(response.Du.tv)
           }}
@@ -33,28 +34,69 @@ function App() {
         />
         : ""}
 
-      <form>
-        <p>
-          Email:
-          <input className='input' name="email" type="text" value={userEmail} onChange={(event) => {
-            setUserEmail(event.target.value)
-          }}/>
-        </p>
+      {isLoggedIn ?
+        <form>
+          <p>
+            Email:
+            <input className='input' name="email" type="text" value={userEmail} onChange={(event) => {
+              setUserEmail(event.target.value)
+            }}/>
+          </p>
 
-        <br/>
+          <br/>
+          <p>
+            Asset URL:
+            <input className='input' name="asset_url" type="text" value={assetUrl} onChange={(event) => {
+              setAssetUrl(event.target.value)
+            }}/>
+          </p>
 
-        <div className='send-form-button'>
-          <button className='button' onClick={() => {
-            alert('נשלח!\n' + userEmail);
-            // send to backend
-          }}>
-            Send!
-          </button>
-        </div>
-      </form>
+          <div className='send-form-button'>
+            <button className='button' onClick={(e) => {
+              e.preventDefault();
+
+              const upsertParams = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({"url": assetUrl, "user_email": userEmail})
+              };
+              fetch('https://alert-scraper.herokuapp.com/upsert_asset/', upsertParams)
+                .then(response => response.json())
+                .then(data => {
+                  console.log('Success:', data);
+                  setAssetUrl("")
+                })
+                .catch((error) => {
+                  console.error('Error:', error);
+                });
+            }}>
+              Send
+            </button>
+
+            <button className='button' onClick={(e) => {
+              e.preventDefault();
+
+              fetch('https://alert-scraper.herokuapp.com/start')
+                .then(response => response.json())
+                .then(data => setAssetsList(data));
+            }}>
+              Start
+            </button>
+
+            <button className='button' onClick={(e) => {
+              e.preventDefault();
+
+              fetch('https://alert-scraper.herokuapp.com/stop')
+                .then(response => response.json())
+                .then(data => setAssetsList(data));
+            }}>
+              Stop
+            </button>
+          </div>
+        </form> :
+        ""}
 
       <p>
-        LIST!
         {assetsList}
       </p>
     </div>
