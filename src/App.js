27753b05ab634ad1e06677ebed08850db01152e4
yuvalta <tamir.yuval1@gmail.com>
2022-03-26 10:20:16 +0300

    Showing list of asset to user

--- src/App.js
+++ src/App.js
@@ -1,149 +1,187 @@
 import './App.css';
 import GoogleLogin from "react-google-login";
 import React, {useEffect, useState} from "react";
+import {
+  Grid,
+  CircularProgress,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  TextField,
+  Fade,
+} from "@material-ui/core";
+
+import logo from "./assets/pelican_logo.png";
+import background_image from "./assets/background.png";
 
 
 function App() {
   const [userEmail, setUserEmail] = useState("");
   const [assetUrl, setAssetUrl] = useState("");
   const [isLoggedIn, setLoggedIn] = useState(false);
-  const [assetsList, setAssetsList] = useState("");
+  const [assetsList, setAssetsList] = useState([]);
+
 
   useEffect(() => {
-    console.log("stam4")
-
-    // // fetch('https://alert-scraper.herokuapp.com/get_assets_for_user?user_email=' + userEmail)
-    // fetch("https://alert-scraper.herokuapp.com/test")
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log(data);
-    //     setAssetsList(JSON.stringify(data))
-    //   }).catch(reason => console.log(reason));
-  }, [userEmail, assetUrl]);
+    getListFromDB(userEmail)
+  }, [userEmail]);
 
   return (
-    <div className="App">
-      {!isLoggedIn ?
-        <GoogleLogin
-          clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
-          buttonText="Login"
-          onSuccess={(response) => {
-            console.log(response.Du.tv);
-            setLoggedIn(true)
-            setUserEmail(response.Du.tv)
-          }}
-          onFailure={(response) => {
-            console.log(response);
-            setUserEmail(response)
-          }}
-          cookiePolicy={'single_host_origin'}
-        />
-        : <form>
-          <p>
-            Email:
-            <input className='input' name="email" type="text" value={userEmail} onChange={(event) => {
-              setUserEmail(event.target.value)
-            }}/>
-          </p>
+    <div className="user-form-container" style={{
+      backgroundImage: `url(${background_image})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      width: '100%',
+      height: '100vh',
+    }}>
+      <Typography variant="h2">
+        Alerter
+      </Typography>
+      <div className="container">
+        {!isLoggedIn ?
+          <GoogleLogin
 
-          <br/>
-          <p>
-            Asset URL:
-            <input className='input' name="asset_url" type="text" value={assetUrl} onChange={(event) => {
-              setAssetUrl(event.target.value)
-            }}/>
-          </p>
+            clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={(response) => {
+              console.log(response.Du.tv);
+              setLoggedIn(true)
+              setUserEmail(response.Du.tv)
+            }}
+            onFailure={(response) => {
+              console.log(response);
+              setUserEmail(response)
+            }}
+            cookiePolicy={'single_host_origin'}
+          />
+          :
+          <form>
+            <p>
+              Email:
+              <input className='input' name="email" type="text" value={userEmail} onChange={(event) => {
+                setUserEmail(event.target.value)
+              }}/>
+            </p>
 
-          <div className='send-form-button'>
-            <button className='button' onClick={(e) => {
-              e.preventDefault();
+            <br/>
+            <p>
+              Asset URL:
+              <input className='input' name="asset_url" type="text" value={assetUrl} onChange={(event) => {
+                setAssetUrl(event.target.value)
+              }}/>
+            </p>
 
-              const upsertParams = {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"url": assetUrl, "user_email": userEmail})
-              };
-              fetch('https://alert-scraper.herokuapp.com/upsert_asset/', upsertParams)
-                .then(response => response)
-                .then(data => {
-                  console.log('Success:', data);
-                  setAssetUrl("")
-                })
-                .catch((error) => {
-                  console.error('Error:', error);
-                });
-            }}>
-              Send
-            </button>
+            <div className='send-form-button'>
+              <button className='button' onClick={(e) => {
+                e.preventDefault();
 
-            <button className='button' onClick={(e) => {
-              e.preventDefault();
+                const upsertParams = {
+                  method: 'POST',
+                  headers: {'Content-Type': 'application/json'},
+                  body: JSON.stringify({"url": assetUrl, "user_email": userEmail})
+                };
+                fetch('https://alert-scraper.herokuapp.com/upsert_asset/', upsertParams)
+                  .then(response => response)
+                  .then(data => {
+                    console.log('Success:', data);
+                    setAssetUrl("")
+                    getListFromDB(userEmail)
+                  })
+                  .catch((error) => {
+                    console.error('Error:', error);
+                  });
+              }}>
+                Send
+              </button>
 
-              fetch('https://alert-scraper.herokuapp.com/start')
-                .then(response => response)
-                .then(data => console.log("start " + data));
-            }}>
-              Start
-            </button>
+              <button className='button' onClick={(e) => {
+                e.preventDefault();
+                console.log('Started:');
+                fetch('https://alert-scraper.herokuapp.com/start')
+                  .then(response => response)
+                  .then(data => console.log("start " + data));
+              }}>
+                Start
+              </button>
 
-            <button className='button' onClick={(e) => {
-              e.preventDefault();
+              <button className='button' onClick={(e) => {
+                e.preventDefault();
 
-              fetch('https://alert-scraper.herokuapp.com/stop')
-                .then(response => response)
-                .then(data => console.log("stop " + data));
-            }}>
-              Stop
-            </button>
+                fetch('https://alert-scraper.herokuapp.com/stop')
+                  .then(response => response)
+                  .then(data => console.log("stop " + data));
+              }}>
+                Stop
+              </button>
 
-            <button className='button' onClick={(e) => {
-              e.preventDefault();
-              fetch('https://alert-scraper.herokuapp.com/test/')
-                .then(body => body.json())
-                .then(data => {
-                  console.log('Success test:', data);
-                })
-                .catch((error) => {
-                  console.error('Error test:', error);
-                });
-            }}>
-              Test
-            </button>
+              <button className='button' onClick={(e) => {
+                e.preventDefault();
+                fetch('https://alert-scraper.herokuapp.com/test/')
+                  .then(body => body.json())
+                  .then(data => {
+                    console.log('Success test:', data);
+                  })
+                  .catch((error) => {
+                    console.error('Error test:', error);
+                  });
+              }}>
+                Test
+              </button>
 
-            <button className='button' onClick={(e) => {
-              e.preventDefault();
+              <button className='button' onClick={(e) => {
+                e.preventDefault();
+                getListFromDB(userEmail)
+              }}>
+                List
+              </button>
+            </div>
+          </form>}
 
-              const params = {
-                user_email: userEmail,
-              };
-              const options = {
-                method: 'POST',
-                body: JSON.stringify(params),
-                headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-                },
-              };
-
-              console.log('before', JSON.stringify(params));
-              fetch('https://alert-scraper.herokuapp.com/get_assets_for_user/', options)
-                .then(response => response.json())
-                .then(response => {
-                  setAssetsList(JSON.stringify(response))
-                  console.log('uvuv', response);
-                });
-            }}>
-              List
-            </button>
-          </div>
-        </form>}
-
-      <p>
-        {assetsList}
-      </p>
+        <div>
+          {Object.entries(assetsList).map(asset =>
+            <a target="_blank"
+               href={stripAssetURL(asset.toString())}>{stripAssetURL(asset.toString())}<br/><br/></a>)}
+        </div>
+      </div>
     </div>
 
   );
+
+  function stripAssetURL(assetURL) {
+    if (assetURL.length > 0) {
+      return assetURL.substring(2)
+    }
+
+    return ""
+  }
+
+  function getListFromDB(userEmail) {
+    const params = {
+      user_email: userEmail,
+    };
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    };
+
+    console.log('params', params);
+
+    fetch('https://alert-scraper.herokuapp.com/get_assets_for_user/', options)
+      .then(response => response.json())
+      .then(response => {
+
+        console.log('uvuv', JSON.parse(JSON.stringify(response)));
+        setAssetsList(JSON.parse(JSON.stringify(response)))
+      });
+  }
+
 }
+
 
 export default App;
---
+++ src/AssetItem.js
@@ -0,0 +1,9 @@
+import React, {useContext} from 'react';
+
+const AssetItem = () => {
+
+  return (<div></div>
+  );
+}
+
+export default AssetItem;
